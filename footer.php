f5f490f90d649b56e22072ca1db76d9aa94de1eb
Swen thuis <swentemebel@outlook.com>
2023-09-24 16:37:03 +0200

    navbar toegevoed, header file in php en footer in php.

---
+++ footer.php
@@ -0,0 +1,12 @@
+<?php
+echo <<<_END
+
+<footer>
+
+  
+
+</footer>
+
+_END;
+
+?>
---
+++ header.php
@@ -0,0 +1,27 @@
+<?php
+echo <<<_END
+
+<header>
+
+    <div class="page-header">
+        <div class="logo">
+            <a href="index.html">Logo.</a>
+        </div>
+
+        <div class="navbar">
+            <ul>
+                <li><a href="index.html">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Skills</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Portfolio</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </div>
+    </div>
+
+</header>
+
+_END
+
+?>
